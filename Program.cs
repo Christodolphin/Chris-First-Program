881a6fdd601415cf8ce460232cf285df86ba90f5
Christodolphin <71910394+Christodolphin@users.noreply.github.com>
2020-09-25 19:37:10 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChrisWorkflowLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           //console.WriteLine();
+            Console.WriteLine("\n\t\"Hello World! Good Night, Sweet Prince.\"-Brian Kernighan + Shakespeare\n");
+           //console.WriteLine();
+        }
+    }
+}
